3ad222ad736c2fc271f482c913ed6e4682c0f4c4
Aleksandr <amvorobei@gmail.com>
2021-10-01 14:35:52 +0300

    add simple form for contacts

--- apps/contacts/views.py
+++ apps/contacts/views.py
@@ -6,7 +6,15 @@ from .serializers import ContactSerializer
 
 class ContactViewSet(ModelViewSet):
     serializer_class = ContactSerializer
-    permission_classes = IsAuthenticated
+    permission_classes = [IsAuthenticated]
+    search_fields = (
+        "first_name",
+        "last_name",
+        "phone",
+        "country",
+        "city",
+        "street",
+    )
 
     def get_queryset(self):
         return Contact.objects.filter(user=self.request.user)
