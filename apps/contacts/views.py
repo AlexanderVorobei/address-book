db6d00bbf5480dbe58a365857f0299b56423ffb7
Aleksandr <amvorobei@gmail.com>
2021-10-01 20:05:20 +0300

    fix index.html

--- apps/contacts/views.py
+++ apps/contacts/views.py
@@ -1,5 +1,6 @@
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.permissions import IsAuthenticated
+from rest_framework import renderers
 from .models import Contact
 from .serializers import ContactSerializer
 
@@ -7,6 +8,7 @@ from .serializers import ContactSerializer
 class ContactViewSet(ModelViewSet):
     serializer_class = ContactSerializer
     permission_classes = [IsAuthenticated]
+    renderer_classes = [renderers.JSONRenderer, renderers.BrowsableAPIRenderer, renderers.TemplateHTMLRenderer]
     search_fields = (
         "first_name",
         "last_name",
